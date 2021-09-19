91c18b068553493a6a8303f3f29ad32e2b638f83
Ian Cornish <ian.cornish1@gmail.com>
2021-09-19 17:42:26 -0500

    Refactored index.js
    Made updates so that index.js is now using settings data and moved injury report code out of the main  handler.

--- AWSVersion/index.js
+++ AWSVersion/index.js
@@ -1,100 +1,19 @@
-var axios = require('axios');
-var weekConfig = require('./weekConfig.js');
-var AWS = require('aws-sdk');
+let axios = require('axios');
+let weekConfig = require('./weekConfig.js');
+let injuryReportGenerator = require('./injuryReport');
+let AWS = require('aws-sdk');
+let settings = require('./settings');
 
-function groupBy(list, keyGetter) {
-    const map = new Map();
-    list.forEach((item) => {
-         const key = keyGetter(item);
-         const collection = map.get(key);
-         if (!collection) {
-             map.set(key, [item]);
-         } else {
-             collection.push(item);
-         }
-    });
-    return map;
-}
 
 exports.handler = function(){
 
 let week = weekConfig();
-let urlBase ='https://fantasy.espn.com/apis/v3/games/ffl/seasons/2021/segments/0/leagues/72628823?view=mMatchup&view=mMatchupScore&scoringPeriodId=';
-let url = urlBase.concat(week);
-
-const teamIdByOwner = [{teamId : "1", teamOwner : "Ian Cornish"},
-                       {teamId : "2", teamOwner : "Richard Stubing"},
-                       {teamId : "3", teamOwner : "Donnie Reynolds"},
-                       {teamId : "4", teamOwner : "Devlin Brooks"},
-                       {teamId : "5", teamOwner : "Freddy Ponce"},
-                       {teamId : "6", teamOwner : "Kyle Hurley"},
-                       {teamId : "7", teamOwner : "Austin Reeves"},
-                       {teamId : "8", teamOwner : "Danny Stubing"},
-                       {teamId : "9", teamOwner : "Chris Serrano"},
-                       {teamId : "10", teamOwner : "Josh Hinrichsen"},
-                       {teamId : "11", teamOwner : "David Goodyear"},
-                       {teamId : "12", teamOwner : "Cody Ponce"}];
-
-const lineupSlotIdByPositionName = [{lineupSlotId : "0", positionName : "Quarterback"},
-                                    {lineupSlotId : "2", positionName : "Running Back"},
-                                    {lineupSlotId : "4", positionName : "Wide Reciever"},
-                                    {lineupSlotId : "6", positionName : "Tight End"},
-                                    {lineupSlotId : "16", positionName : "D/ST"},
-                                    {lineupSlotId : "17", positionName : "Kicker"},
-                                    {lineupSlotId : "20", positionName : "Bench"},
-                                    {lineupSlotId : "21", positionName : "IR"},
-                                    {lineupSlotId : "23", positionName : "Flex"}];
+let url = 'https://fantasy.espn.com/apis/v3/games/ffl/seasons/'+settings.seasonId+'/segments/0/leagues/'+settings.leagueId+'?view=mMatchup&view=mMatchupScore&scoringPeriodId='+week;
 
 async function compileHtmlOutput(){ return axios.get(url).then(async (res)=> 
 {   
-    let responseTeams = res.data.teams;
-    
-        let teamRosters = [];
-        for(let i of responseTeams){
-            teamRosters.push(i.roster);
-        }
-        let listOfRosterEntries = [];
-        for(let j of teamRosters){
-            for(let l = 0; l< j.entries.length; l++){
-                listOfRosterEntries.push(j.entries[l]);
-            }
-        }
-        const playersOnFantasyRosters = listOfRosterEntries.map(entry => {
-
-            let ownerNameValue = teamIdByOwner.filter(team => team.teamId == entry.playerPoolEntry.onTeamId);
-            let positionNameValue = lineupSlotIdByPositionName.filter(position => position.lineupSlotId == entry.lineupSlotId);
-
-            return{playerFullName : entry.playerPoolEntry.player.fullName,
-                   ownerName : ownerNameValue[0].teamOwner,
-                   injuryStatus : entry.playerPoolEntry.player.injuryStatus,
-                   position : positionNameValue[0].positionName,
-                   playerId : entry.playerId,
-                   currentLineupSlotId : entry.lineupSlotId,
-                   teamId : entry.playerPoolEntry.onTeamId
-                   };
-        });
-
-        const getInactiveStartingPlayers = playersOnFantasyRosters.filter(player => player.position != "Bench" && player.position != "IR" && player.position != "D/ST" && player.injuryStatus != "ACTIVE" && player.injuryStatus != "INJURY_RESERVE");
-        
-        const startingPlayersWithStatusByOwner = new Map([...groupBy(getInactiveStartingPlayers, player => player.ownerName).entries()].sort()); 
-        var htmlOutput = '<!DOCTYPE html> <html><style> #players { font-family: Arial, Helvetica, sans-serif; border-collapse: collapse;} #players td, #players th { border: 1px }</style> <body> <table id="players"> <tr> <td style="text-align: center;background-color: #04AA6D;color: white;" colspan = "3"><b>Week  ' + week + ' Injury Report</b></td> </tr> ';
-
-        for(let [key, value] of startingPlayersWithStatusByOwner){
-            htmlOutput = htmlOutput.concat('<tr style="background-color: #ddd !important"> <td colspan = "5"> <b>' +key+ '</b> </td> </tr>')
-            for(const player of startingPlayersWithStatusByOwner.get(key)){
-                htmlOutput= htmlOutput.concat(' <tr> ');
-                if(player.injuryStatus=='OUT' || player.injuryStatus=='INJURY_RESERVE'){
-                    htmlOutput = htmlOutput.concat('<td style="color:red; font-weight: bold;"> ' + player.playerFullName + ' </td> <td style="color:red; font-weight: bold;"> ' + player.injuryStatus +' </td>');
-                }else if(player.injuryStatus=='DOUBTFUL'){
-                    console.log(player.playerFullName);
-                    htmlOutput = htmlOutput.concat('<td style="color:orange; font-weight: bold;"> ' + player.playerFullName + ' </td> <td style="color:orange; font-weight: bold;"> ' + player.injuryStatus +' </td>');
-                }else{
-                    htmlOutput = htmlOutput.concat('<td> ' + player.playerFullName + ' </td> <td> ' + player.injuryStatus +' </td>');
-                }
-                htmlOutput= htmlOutput.concat(' </tr> ');
-            }
-        }
-        htmlOutput = htmlOutput.concat(' </table> </body> </html>');
+    let htmlOutput = injuryReportGenerator(res.data.teams,week);
+    console.log(htmlOutput);
 return htmlOutput;}    
 );
     };
