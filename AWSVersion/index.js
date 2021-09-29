0a069a48bf7e1f2f3c22cfc2f0e8e8b407c96e1b
Ian Cornish <ian.cornish1@gmail.com>
2021-09-29 16:26:15 -0500

    Refactored code

--- AWSVersion/index.js
+++ AWSVersion/index.js
@@ -1,6 +1,7 @@
 let axios = require('axios');
 let weekConfig = require('./weekConfig.js');
 let injuryReportGenerator = require('./injuryReport');
+let matchupHistory = require('./matchupHistory');
 let emailer = require('./emailer');
 let settings = require('./settings');
 
@@ -12,13 +13,16 @@ let url = 'https://fantasy.espn.com/apis/v3/games/ffl/seasons/'+settings.seasonI
 
 async function compileHtmlOutput(){ return axios.get(url).then(async (res)=> 
 {   
-    let htmlOutput = injuryReportGenerator(res.data.teams,week);
-    console.log(htmlOutput);
+    let htmlOutput='<!DOCTYPE html> <html><body><div style="text-align:center;font-family: Arial, Helvetica, sans-serif;"><h1>Preview</h1>';
+    let injuryReport = await injuryReportGenerator(res.data.teams,week);
+    let matchupHistoryReport = await matchupHistory(res.data, week);
+    htmlOutput = htmlOutput.concat(matchupHistoryReport + '</div"><br><br><br><div style="text-align:center;font-family: Arial, Helvetica, sans-serif;"><h1>Injury Report</h1>');
+    htmlOutput = htmlOutput.concat(injuryReport + '</div></body></html>');
 return htmlOutput;}    
 );
     };
 let report = compileHtmlOutput().then(async (htmlOutput)=>{
-    return emailer(htmlOutput, week);
+    return emailer(htmlOutput, week).catch(()=>(console.log(htmlOutput)));
 });
 return report;
 };
