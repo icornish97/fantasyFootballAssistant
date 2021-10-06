2a11f2e467fc9591dc4a89d595afa76bbfd33c0b
Ian Cornish <ian.cornish1@gmail.com>
2021-10-05 20:20:53 -0500

    Refactor

--- AWSVersion/previousWeekRecap.js
+++ AWSVersion/previousWeekRecap.js
@@ -2,10 +2,12 @@ let utils = require('./utils');
 let settings = require('./settings');
 
 module.exports = async function recapPriorWeek(data, week){ 
-    let priorGames = data.schedule.filter(game => game.matchupPeriodId == week-1).map(game=> {return utils.decideWinner(game, settings.seasonId)}); 
+    let priorGames = data.schedule.filter(game => game.matchupPeriodId == week-1).map(game=> {return utils.decideWinner(game, settings.seasonId, week)}); 
+    console.log(priorGames);
     let scores = await getScores(priorGames);
     let high = await getHigh(scores); 
-    let low = await getLow(scores);   
+    let low = await getLow(scores); 
+    let avg = await getAverage(scores);
     let htmlOutput = '<style> #recap { font-family: Arial, Helvetica, sans-serif; border-collapse: collapse;margin-left:auto; margin-right:auto;} #recap td, #recap th { border: 1px }</style> <body> <table id="recap"> <tr> <td style="text-align: center;background-color: #04AA6D;color: white;" colspan = "7"><b>Recap</b></td> </tr><th style="text-align:center;padding-right:5px;padding-left:5px">Winner</th><th style="text-align:center;padding-right:5px;padding-left:5px">Points</th><th>Loser</th><th style="text-align:center;padding-right:5px;padding-left:5px">Points</th>';
     let even = 0;
     for(i of priorGames){
@@ -24,6 +26,7 @@ module.exports = async function recapPriorWeek(data, week){
             }   
         }
     }
+    htmlOutput = htmlOutput.concat('<tr><td style="text-align:center" colspan = "7"><b>League Average for Week: '+avg+'</b></td></tr>');
     htmlOutput = htmlOutput.concat("</table>");
     return htmlOutput;
 }
@@ -48,6 +51,13 @@ async function getLow(results){
         }
     }
     return lowTeam;
+}
+async function getAverage(results){
+    let sum = 0;
+    for(i of results){
+        sum += i.points;
+    }
+    return Math.round((sum/results.length)*100)/100;
 }
 async function getScores(matchups){
     let teamResults = [];
