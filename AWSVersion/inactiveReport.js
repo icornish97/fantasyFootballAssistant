77c6621b22607f4634e60c7a56b8bcf31bfbb4ff
Ian Cornish <ian.cornish1@gmail.com>
2021-10-15 20:33:49 -0500

    Added Bye Week Support
    The report will now include players that are on a bye week that are in active lineups. Updated name to better represent the file post update.

---
+++ AWSVersion/inactiveReport.js
@@ -0,0 +1,80 @@
+let axios = require('axios');
+let settings = require('./settings');
+let utils = require('./utils');
+
+module.exports = async function generateInactiveReport(responseTeams, week){
+    let listOfRosterEntries = utils.getListOfPlayers(responseTeams);
+    let listOfTeamsWithGame = await getTeamsWithGame(week);
+        const playersOnFantasyRosters = listOfRosterEntries.map(entry => {
+            let ownerNameValue = settings.teamIdByOwner.filter(team => team.teamId == entry.playerPoolEntry.onTeamId);
+            let positionNameValue = settings.lineupSlotIdByPositionName.filter(position => position.lineupSlotId == entry.lineupSlotId);
+
+            return{playerFullName : entry.playerPoolEntry.player.fullName,
+                   ownerName : utils.getOwnerNameByTeamID(entry.playerPoolEntry.onTeamId),
+                   injuryStatus : entry.playerPoolEntry.player.injuryStatus,
+                   position : positionNameValue[0].positionName,
+                   playerId : entry.playerId,
+                   currentLineupSlotId : entry.lineupSlotId,
+                   teamId : entry.playerPoolEntry.onTeamId,
+                   proTeamId : entry.playerPoolEntry.player.proTeamId,
+                   onBye : !checkForGameThisWeek(entry, listOfTeamsWithGame)
+                   };
+        });
+
+        const getInactiveStartingPlayers = playersOnFantasyRosters.filter(player => player.position != "Bench" && player.position != "IR" && player.injuryStatus != "ACTIVE" && player.injuryStatus != "INJURY_RESERVE" && player.injuryStatus != undefined);
+        
+        const startingPlayersWithStatusByOwner = new Map([...groupBy(getInactiveStartingPlayers, player => player.ownerName).entries()].sort()); 
+        var htmlOutput = '<style> #players { font-family: Arial, Helvetica, sans-serif; border-collapse: collapse; margin-left:auto; margin-right:auto;} #players td, #players th { border: 1px }</style> <table id="players"> <tr> <td style="text-align: center;background-color: #04AA6D;color: white;" colspan = "3"><b>Week  ' + week.scoringPeriod + ' Injury Report</b></td> </tr> ';
+
+        for(let [key, value] of startingPlayersWithStatusByOwner){
+            htmlOutput = htmlOutput.concat('<tr style="background-color: #ddd !important"> <td colspan = "5"> <b>' +key+ '</b> </td> </tr>')
+            for(const player of startingPlayersWithStatusByOwner.get(key)){
+                htmlOutput= htmlOutput.concat(' <tr> ');
+                if(player.onBye == true){
+                    htmlOutput = htmlOutput.concat('<td style="color:red; font-weight: bold;"> ' + player.playerFullName + ' </td> <td style="color:red; font-weight: bold;"> BYE </td>');
+                }else{
+                    if(player.injuryStatus=='OUT' || player.injuryStatus=='INJURY_RESERVE'){
+                        htmlOutput = htmlOutput.concat('<td style="color:red; font-weight: bold;"> ' + player.playerFullName + ' </td> <td style="color:red; font-weight: bold;"> ' + player.injuryStatus +' </td>');
+                    }else if(player.injuryStatus=='DOUBTFUL'){
+                        htmlOutput = htmlOutput.concat('<td style="color:orange; font-weight: bold;"> ' + player.playerFullName + ' </td> <td style="color:orange; font-weight: bold;"> ' + player.injuryStatus +' </td>');
+                    }else if(player.injuryStatus != undefined){
+                        htmlOutput = htmlOutput.concat('<td> ' + player.playerFullName + ' </td> <td> ' + player.injuryStatus +' </td>');
+                    }
+                    htmlOutput= htmlOutput.concat(' </tr> ');
+                }
+            }
+        }
+        htmlOutput = htmlOutput.concat(' </table> ');
+        return htmlOutput;
+    }
+
+    function groupBy(list, keyGetter) {
+        const map = new Map();
+        list.forEach((item) => {
+             const key = keyGetter(item);
+             const collection = map.get(key);
+             if (!collection) {
+                 map.set(key, [item]);
+             } else {
+                 collection.push(item);
+             }
+        });
+        return map;
+    }
+
+    function checkForGameThisWeek(playerEntry, teamsWithGameThisWeek){
+        let hasGame = teamsWithGameThisWeek.filter(team => team.id == playerEntry.playerPoolEntry.player.proTeamId);
+        return hasGame.length > 0;
+    }
+
+    async function getTeamsWithGame(week){
+        let url = "https://site.api.espn.com/apis/fantasy/v2/games/ffl/games?dates="+week.startDate.getFullYear() + '' + (week.startDate.getMonth()+1) + '' + week.startDate.getDate()+"-"+week.endDate.getFullYear() + '' + (week.endDate.getMonth()+1) + '' + (week.endDate.getDate()+1)+"&pbpOnly=true";
+        let response = await axios.get(url);
+        let teamsWithGame = [];
+        for(let i = 0; i<response.data.events.length; i++){
+            for(let j = 0; j<response.data.events[i].competitors.length; j++){
+                teamsWithGame.push(response.data.events[i].competitors[j]);
+            }
+        }
+        return teamsWithGame;
+    }
