cf2259af0d785b4f446c3bf0f05b8e370c76e246
Ian Cornish <ian.cornish1@gmail.com>
2023-10-03 15:20:26 -0500

    Updated endpoint to query more data

--- AWSVersion/reportBuilder.js
+++ AWSVersion/reportBuilder.js
@@ -10,20 +10,31 @@ let utils = require('./utils');
 
 module.exports = async function initializeReport(){
     let weekInfo = weekConfig();
-    let url = 'https://fantasy.espn.com/apis/v3/games/ffl/seasons/'+settings.seasonId+'/segments/0/leagues/'+settings.leagueId+'?view=mMatchup&view=mMatchupScore&scoringPeriodId='+weekInfo.scoringPeriod;
-    return axios.get(url).then(async (res)=> 
-        {      
-        return buildReport(res.data, weekInfo);}    
-        );
-}
+    let url = 'https://fantasy.espn.com/apis/v3/games/ffl/seasons/'+settings.seasonId+'/segments/0/leagues/'+settings.leagueId+'?view=mMatchup&view=mTeam&view=mMatchupScore&view=mTopPerformers&scoringPeriodId='+weekInfo.scoringPeriod;
 
+
+    return axios.get(url,{headers:{Cookie:`espn_s2=${settings.espn_s2}; SWID=${settings.SWID};`}}).then(async (res)=>{ leagueDataFromAPI = res.data; return buildReport(res.data, weekInfo);});
+}
+  
 async function buildReport(data, week){
     let htmlOutput=utils.createReport();
-    htmlOutput = htmlOutput.concat(utils.createReportBlock("Recap", await previousWeekRecap(data, week.scoringPeriod)));
-    htmlOutput = htmlOutput.concat(utils.createReportBlock("Stats", await statistics(data, week.scoringPeriod)));
-    htmlOutput = htmlOutput.concat(utils.createReportBlock("Preview", await matchupHistory(data, week.scoringPeriod)));
-    htmlOutput = htmlOutput.concat(utils.createReportBlock("Inactive Report", await inactiveReportGenerator(data.teams,week)));
+    let tempTeamsList = [];
+    //Create map of members 
+    
+    initializeLeagueMembers();
+    weekConfig();
+    htmlOutput = htmlOutput.concat(utils.createReportBlock("Inactive Report", await inactiveReportGenerator(leagueDataFromAPI.teams,currentWeek)));
     htmlOutput = htmlOutput.concat(utils.endReport());
     return htmlOutput;   
 }
 
+function initializeLeagueMembers(){
+    let memberMap = new Map();
+    for(let currentMember of leagueDataFromAPI.members){
+        memberMap.set(currentMember.id, currentMember.firstName + ' ' + currentMember.lastName);
+    }
+    for(let currentTeam of leagueDataFromAPI.teams){
+        currentTeam.ownerName = memberMap.get(currentTeam.primaryOwner);
+        leagueMembers.push(currentTeam);
+    }
+}
